4e1081f5ad06e5d8b95e8a94a2d42e53d25c2cc3
Lovette Duke <qutyluv@gmail.com>
2024-03-23 16:16:30 +0100

    User c

--- models/user.py
+++ models/user.py
@@ -3,8 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -16,7 +14,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-    reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+    
