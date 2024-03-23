424cab47c0e8aa64ef78578d0d466a26d1ab55b7
Lovette Duke <qutyluv@gmail.com>
2024-03-23 16:08:15 +0100

    User b

--- models/user.py
+++ models/user.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.Envar import HBNB_TYPE_STORAGE, DB
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.place import Place
+from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
+
 
 
 class User(BaseModel, Base):
     """This defines the user attributes"""
     __tablename__ = "users"
-    if getenv(HBNB_TYPE_STORAGE) == DB:
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
                               cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
+    reviews = relationship('Review', backref='user',
                                cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
