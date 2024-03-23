78660d3560c48a874605c5d57b0aa0eefa61c74d
Lovette Duke <qutyluv@gmail.com>
2024-03-23 16:04:59 +0100

    Base again

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
@@ -17,7 +18,6 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -51,7 +51,6 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         model.storage.new(self)
         model.storage.save()
@@ -73,5 +72,4 @@ class BaseModel:
         """
         delete object
         """
-        from models import storage
         model.storage.delete(self)
