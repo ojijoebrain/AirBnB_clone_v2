9b39635a3c778437ff3e2f187db60dd6baeb825e
ojijoebrain <ojijosephoji@gmail.com>
2024-04-20 03:06:22 -0700

    task_5_update

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+start Flask application
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C {}".format(text.replace("_", " "))
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_text(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('number_template.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
